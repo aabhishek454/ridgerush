2d7f919159811a49c3acd571236addf71036b5bb
aabhishek454 <164495596+aabhishek454@users.noreply.github.com>
2026-08-22 09:33:38 +0530

    Add RidgeRush game build - batch 1/5

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ridgerush-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();if(e.request.method==='GET'&&res.status===200)caches.open(CACHE).then(cache=>cache.put(e.request,c));return res;}).catch(()=>caches.match('./index.html'))));});
